239f132ea3ff4ce95410209442b57c2fab746f4e
msrahman <msrahman@sfu.ca>
2020-07-31 05:03:10 -0700

    add course enroll functionality

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,5 @@ app_name = 'courses'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:course_id>/enroll/', views.enroll, name='enroll')
 ]
--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Course
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def index(request):
+def index(request, newContext={}):
+    if(request.user.is_authenticated):
+        print(request.user.id)
+    else:
+        print("NO USER LOGGED IN")
     courses = Course.objects.all()
     context = {
         'courses': courses
     }
+    context.update(newContext)
     return render(request, 'courses/index.html', context)
+
+@login_required
+def enroll(request, course_id):
+    course = Course.objects.get(id=course_id)
+    print(request.user)
+    if(course not in request.user.course_set.all()):
+        course.users.add(request.user)
+        course.save()
+        success_msg = "Enrolled"+course.name+"successfully!!"
+    else:
+        success_msg = "Course already enrolled"
+
+    return index(request, {'success_msg':success_msg})
+    # if(request.user.is_authenticated):
